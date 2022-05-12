66179f1448061a78c91d98102f13f9bf18940164
MSeiler-png <61123708+MSeiler-png@users.noreply.github.com>
2022-05-12 10:47:51 +0200

    Create OpenLibrarySearchParams.cs

---
+++ HelperClasses/OpenLibrarySearchParams.cs
@@ -0,0 +1,12 @@
+public struct OpenLibrarySearchParams
+{
+  public string? Title {get; init;}
+
+  public string? Author {get;init;}
+
+  public string? Place {get; init;}
+  
+  public string? Language {get; init;}
+  
+  public string? Harper {get; init;}
+}
